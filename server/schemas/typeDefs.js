5bcaf11298ee0495199d3f669b4e0956b5c1feb9
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-03-23 16:41:32 -0400

    replaced express routes with graph ql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,55 @@
 const { gql } = require('apollo-server-express');
 
 //typedefs
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id:ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+        
+    }
+
+    type Book {
+        bookId:ID!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+        
+    }
+
+    type Auth{
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+      }
+
+    type Query{
+        me: User
+    }
+
+    type Mutation{
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+
+
+
+
+`;
 
 //export the typeDefs
 module.exports = typeDefs;
